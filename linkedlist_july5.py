a5fafb37f5cd23e3edd2512c34c57d39408971f3
Anusha <anusgowda050@gmail.com>
2022-07-05 13:59:40 +0530

    adding comments

--- linkedlist_july5.py
+++ linkedlist_july5.py
@@ -3,7 +3,7 @@ class Node:
        self.data = data
        self.next = None
  
- 
+#this class is for creating a linked list
 class Linkedlist:
     def __init__(self):
         self.head = None
@@ -17,8 +17,8 @@ class Linkedlist:
             self.last_node.next = Node(data)
             self.last_node = self.last_node.next
  
- 
-def first_common(llist1, llist2):
+# Elements that are common in linkedlist llist1 and llist2.
+def common_element(llist1, llist2):
     current1 = llist1.head
     while current1:
         data = current1.data
@@ -33,22 +33,26 @@ def first_common(llist1, llist2):
 llist1 = Linkedlist()
 llist2 = Linkedlist()
 
+
 data_list1 = [3,7,10,15,16,9,22,17,32,3]
+#Inserting element to a first linked list. 
 for data in data_list1:
     llist1.append(int(data))
 
 data_list2 = [16,2,9,13,37,8,10,1,28,2]
+#Inserting element to a second linked list.
 for data in data_list2:
     llist2.append(int(data))
 
-common = first_common(llist1, llist2)
+#Calling function by passing parameter 
+common = common_element(llist1, llist2)
 
 if common:
     print('The two lists haveing common elements are {}.'.format(common))
 else:
     print('The two lists have no common elements.')
     
-
+#contains all elements of l1 as well as l2 ensuring that there is no repeation of elements.
 for x in data_list2:
   data_list1.append(x)
 
@@ -57,24 +61,3 @@ print(data_list1)
 print('After concatinating 2 list with out duplicate value' ,list(set(data_list1)))    
     
     
-    
-class Solution:
-    def solve(self, L1, L2):
-        if not L1:
-            return L2
-        if not L2:
-            return L1
-        
-        
-        # if L1.head < L2.head:
-        #     res = L1
-        #     res.next = self.solve(L1.next, L2)
-        # elif L2 < L1:
-        #     res = L2
-        #     res.next = self.solve(L2.next, L1)
-        # else:
-        #     res = L1
-        #     res.next = self.solve(L1.next, L2.next)
-        return 
-ob = Solution()
-print(ob.solve(llist1, llist2))
